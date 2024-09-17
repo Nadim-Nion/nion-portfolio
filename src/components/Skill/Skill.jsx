baf8c44863ef6155cd07a7f2bc98fbb46ffa0c9b
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2024-09-17 18:46:43 +0600

    skill section added

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import SkillCard from '../SkillCard/SkillCard';
 
 const Skill = () => {
+    const [skills, setSkills] = useState([]);
+    useEffect(() => {
+        fetch('skills.json')
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                setSkills(data);
+            })
+    }, []);
+
     return (
-        <div>
-            <h2>Skill</h2>
+        <div className='max-w-7xl mx-auto p-7 mb-10'>
+            <h2 className='text-5xl font-bold text-white text-center'>Skills</h2>
+            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6'>
+                {
+                    skills.map(skill => <SkillCard
+                        key={skill.name}
+                        skill={skill} />
+                    )
+                }
+            </div>
         </div>
     );
 };
---
+++ src/components/SkillCard/SkillCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const skillColors = {
+    HTML: 'shadow-red-500',
+    CSS: 'shadow-sky-500',
+    React: 'shadow-blue-500',
+    'Tailwind CSS': 'shadow-sky-400',
+    'DaisyUI': 'shadow-purple-500',
+    JavaScript: 'shadow-yellow-500',
+    'Node.js': 'shadow-green-500',
+    'Express.js': 'shadow-gray-700',
+    MongoDB: 'shadow-green-600',
+    Stripe: 'shadow-blue-600',
+    'JSON Web Token': 'shadow-indigo-500',
+    git: 'shadow-orange-600',
+    GitHub: 'shadow-gray-800',
+    Vercel: 'shadow-gray-600',
+    Netlify: 'shadow-cyan-500',
+    Surge: 'shadow-gray-500',
+    'VS Code': 'shadow-blue-400',
+    Figma: 'shadow-pink-400',
+};
+
+const SkillCard = ({ skill }) => {
+    const { name, image } = skill;
+    const shadowColor = skillColors[name] || 'shadow-gray-300';
+
+    return (
+        <div className={`p-4 rounded-lg ${shadowColor} shadow-lg hover:shadow-xl transition-shadow duration-300`}>
+            <img
+                src={image}
+                alt={image}
+                className="rounded-xl w-[90px] h-[90px] mx-auto mb-3" />
+            <h2 className="text-center text-lg font-semibold">{name}</h2>
+        </div>
+    );
+};
+
+export default SkillCard;
