3bbfcb77ca42aee42761d9098cbbca2d713f07fa
Nadim Mahmud Nion <nadim15-1746@diu.edu.bd>
2025-06-29 18:33:47 +0600

    overflow hidden on mobile screen fixed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -19,28 +19,6 @@ const Navbar = () => {
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  // const navItems = (
-  //   <>
-  //     {["Home", "About", "Skill", "Projects", "Education", "Contact"].map(
-  //       (text) => (
-  //         <li key={text}>
-  //           <ScrollLink
-  //             to={text.toLowerCase()}
-  //             smooth={true}
-  //             duration={500}
-  //             offset={-80}
-  //             className="cursor-pointer group relative text-white px-2 py-1"
-  //             onClick={() => setMobileOpen(false)} // close mobile nav
-  //           >
-  //             {text}
-  //             <span className="absolute left-0 -bottom-1 w-full h-[2px] bg-gradient-to-r from-purple-500 via-pink-500 to-blue-500 scale-x-0 group-hover:scale-x-100 transition-transform origin-left"></span>
-  //           </ScrollLink>
-  //         </li>
-  //       )
-  //     )}
-  //   </>
-  // );
-
   return (
     <header
       className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar/Navbar';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 const MainLayout = () => {
-    return (
-        <div className="relative min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
-            <Navbar></Navbar>
-            <div className='grow'>
-                <Outlet></Outlet>
-            </div>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div className="relative min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] overflow-x-hidden">
+      <Navbar></Navbar>
+      <div className="grow">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
